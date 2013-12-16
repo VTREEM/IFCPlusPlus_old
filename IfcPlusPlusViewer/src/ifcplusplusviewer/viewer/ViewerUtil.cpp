efbde34ae01af924ef83065e1c42502498120d9f
Sylvain MARIE <sylvain.marie@vtreem.com>
2013-12-16 23:03:52 +0100

    [Qa] Fix build errors when compiling IFCPlusPlusViewer

--- IfcPlusPlusViewer/src/ifcplusplusviewer/viewer/ViewerUtil.cpp
+++ IfcPlusPlusViewer/src/ifcplusplusviewer/viewer/ViewerUtil.cpp
@@ -9,14 +9,14 @@
 void intersectRayWithPlane( const osg::Vec3d& ray_origin, const osg::Vec3d& ray_direction, const IntersectionPlane& plane, osg::Vec3d& intersection_point )
 {
 	double n = 0;
-	if( plane == IntersectionPlane::XY_PLANE )
+	if( plane == XY_PLANE )
 	{
 		// intersect mouse ray with x-y-plane
 		// g = vec1 + n*(vec2-vec1),  z = 0
 		// 0 = vec1.z + n*(vec2.z - vec1.z)
 		n = -ray_origin.z() / (ray_direction.z());
 	}
-	else if( plane == IntersectionPlane::XZ_PLANE )
+	else if( plane == XZ_PLANE )
 	{
 		n = -ray_origin.y() / (ray_direction.y());
 	}
