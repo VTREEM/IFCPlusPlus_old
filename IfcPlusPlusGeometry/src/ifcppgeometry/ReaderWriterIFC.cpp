c5618032ae5a19fd6b740573eae40f9200ec2e89
Sylvain MARIE <sylvain.marie@vtreem.com>
2014-01-09 18:51:58 +0100

    [Qa] Bad merge(?) : Filter in/out IFC types even if we’re not using OpenMP

--- IfcPlusPlusGeometry/src/ifcppgeometry/ReaderWriterIFC.cpp
+++ IfcPlusPlusGeometry/src/ifcppgeometry/ReaderWriterIFC.cpp
@@ -450,7 +450,30 @@ void ReaderWriterIFC::createGeometry()
 
 		try
 		{
-			convertIfcProduct( product, product_shape );
+			// (Compare types in lowercase to avoid typos)
+			std::string lowercaseType = product->classname();
+			for (std::string::iterator c = lowercaseType.begin(); c != lowercaseType.end(); ++c)
+			{
+				*c = tolower(*c);
+			}
+
+			if (std::find(m_ignored_types.begin(),  m_ignored_types.end(),  lowercaseType) == m_ignored_types.end())
+			{
+				std::cerr << "Type " << lowercaseType << " not in Ignore list" << std::endl;
+			}
+
+			if ((m_selected_types.size() != 0) && (std::find(m_selected_types.begin(), m_selected_types.end(), lowercaseType) != m_selected_types.end()))
+			{
+				std::cerr << "Type " << lowercaseType << " in Select list" << std::endl;
+			}
+
+			// Filter out element types found in m_ignored_types, select elements found in m_selected_types
+			if ((std::find(m_ignored_types.begin(),  m_ignored_types.end(),  lowercaseType) == m_ignored_types.end()) &&
+				((m_selected_types.size() == 0) || (std::find(m_selected_types.begin(), m_selected_types.end(), lowercaseType) != m_selected_types.end())))
+			{
+				// Build the shape representing the product, if required
+				convertIfcProduct( product, product_shape );
+			}
 		}
 		catch( IfcPPException& e)
 		{
